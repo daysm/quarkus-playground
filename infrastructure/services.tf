09219e5136324c2c0674a872793a662b8411ca92
Dayyan Smith <7551272+daysm@users.noreply.github.com>
2025-04-19 23:38:48 +0200

    Fix prod build (#11)
    * Try fixing prod build
    
    * WIP
    
    * Reset triggers

--- infrastructure/services.tf
+++ infrastructure/services.tf
@@ -1,3 +1,7 @@
 resource "google_project_service" "artifact_registry" {
   service = "artifactregistry.googleapis.com"
 }
+
+resource "google_project_service" "iam_credentials" {
+  service = "iamcredentials.googleapis.com"
+}
