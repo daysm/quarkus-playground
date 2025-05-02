a610880a7fd64766f7b2f190fd5ff490b7ddc067
Dayyan Smith <dayyan.smith@gmail.com>
2025-05-02 16:27:02 +0200

    Allow GitHub Actions to access Secret Manager

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -41,6 +41,7 @@ resource "google_project_iam_member" "github_sa_permissions" {
     "roles/iam.workloadIdentityPoolAdmin",
     "roles/storage.admin",
     "roles/run.admin",
+    "roles/secretmanager.secretAccessor"
   ])
 
   project = var.gcp_project_id
