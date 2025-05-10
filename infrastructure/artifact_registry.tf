62a9cea00b88a7df7b28142c0721fcf0bc0f6e32
Dayyan Smith <7551272+daysm@users.noreply.github.com>
2025-05-10 16:20:45 +0200

    Add cleanup policy to artifact registry repository (#22)

--- infrastructure/artifact_registry.tf
+++ infrastructure/artifact_registry.tf
@@ -5,4 +5,35 @@ resource "google_artifact_registry_repository" "docker_repo" {
   repository_id = "images-${var.env}"
   description   = "Docker repository for container images"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-deployed-versions"
+    action = "KEEP"
+    condition {
+      tag_prefixes          = [var.image_tag]
+      tag_state             = "TAGGED"
+      package_name_prefixes = ["app"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-most-recent-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count            = var.additional_images_to_keep_count
+      package_name_prefixes = ["app"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-old-versions"
+    action = "DELETE"
+    condition {
+      # Delete versions of 'app' images older than the specified duration
+      # IF AND ONLY IF they were NOT kept by the preceding 'KEEP' policies.
+      older_than            = var.delete_images_older_than
+      package_name_prefixes = ["app"]
+      tag_state             = "ANY"
+    }
+  }
 }
--- infrastructure/vars/dev.tfvars
+++ infrastructure/vars/dev.tfvars
@@ -1,5 +1,7 @@
-gcp_project_id = "quarkus-playground-dev"
-gcp_region     = "us-east1"
-env            = "dev"
-git_repository = "daysm/quarkus-playground"
-image_tag      = "latest" # Placeholder - will be overridden by GitHub Actions
+gcp_project_id                  = "quarkus-playground-dev"
+gcp_region                      = "us-east1"
+env                             = "dev"
+git_repository                  = "daysm/quarkus-playground"
+image_tag                       = "latest" # Placeholder - will be overridden by GitHub Actions
+additional_images_to_keep_count = 3
+delete_images_older_than        = "3d"
